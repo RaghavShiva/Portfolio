1ffbfd68b2885be1c56dc47615797251755de6a1
RaghavShiva <shiva26122004@gmail.com>
2024-06-16 21:27:26 +0530

    contact info saved

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FaInstagram } from "react-icons/fa";
-import { FaFacebook } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithubSquare } from "react-icons/fa";
@@ -13,22 +12,19 @@ const contact = () => {
       <div className="contact-icon"
       data-aos="zoom-in-up"
       data-aos-duration = "1000">
-        <a href="#" target="_blank" className="items">
+        <a href="https://www.instagram.com/ragshiv04" target="_blank" className="items">
           <FaInstagram className='icons'/>
         </a>
-        <a href="#" target="_blank" className="items">
-          <FaFacebook className='icons'/>
-        </a>
-        <a href="#" target="_blank" className="items">
+        <a href="https://www.linkedin.com/in/shiva-raghav-96b94a255?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" className="items">
         <FaLinkedin className='icons'/>
         </a>
-        <a href="#" target="_blank" className="items">
+        <a href="https://x.com/raghav_shiva04" target="_blank" className="items">
         <FaSquareXTwitter className='icons'/>
         </a>
-        <a href="https://www.github.com" target="_blank" className="items">
+        <a href="https://github.com/RaghavShiva" target="_blank" className="items">
           <FaGithubSquare className='icons'/>
           </a>
-          <a href="mailto:mailid@gmail.com" target="_blank" className="items">
+          <a href="mailto:raghavshiva97.6@gmail.com" target="_blank" className="items">
           <CgMail className='icons'/>
           </a>
       </div>
